82f111586567721b14b269592e63923a2a5bf6d8
Akhankshajupalli <jupalliakhanksha@gmail.com>
2025-02-14 15:26:38 +0530

    Assignment 2 is updated

--- features/features/src/App.jsx
+++ features/features/src/App.jsx
@@ -1,11 +1,21 @@
-import React from "react";
-import Home from "./components/layouts/Home";
-
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import './components/styles/home.css';
+import './components/styles/header.css';
+import './components/styles/footer.css';
+import Header from './components/layouts/Header';
+import Home from './components/layouts/Home';
+import Footer from './components/layouts/footer'; // Correct casing for Footer
 const App = () => {
   return (
-    <div className="App">
-      <Home />
-    </div>
+    <Router>
+      <div className="app">
+        <Header />
+        <Routes>
+          <Route path="/" element={<Home />} /> {/* Home is now the default route */}
+        </Routes>
+        <Footer />
+      </div>
+    </Router>
   );
 };
 
