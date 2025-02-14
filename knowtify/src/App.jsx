83ba73021ed63d8bdf1d4e5f2ddaa547d31c239f
Akhankshajupalli <jupalliakhanksha@gmail.com>
2025-02-14 21:13:34 +0530

    .

---
+++ knowtify/src/App.jsx
@@ -0,0 +1,24 @@
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import './components/styles/home.css';
+import './components/styles/header.css';
+import './components/styles/footer.css';
+import Header from './components/layouts/Header';
+import Home from './components/layouts/Home';
+import Footer from './components/layouts/footer'; // Correct casing for Footer
+
+const App = () => {
+  return (
+    <Router>
+      <div className="app">
+        <Header />
+        <Routes>
+          <Route path="/" element={<Home />} /> {/* Default route for Home */}
+          
+        </Routes>
+        <Footer />
+      </div>
+    </Router>
+  );
+};
+
+export default App;
