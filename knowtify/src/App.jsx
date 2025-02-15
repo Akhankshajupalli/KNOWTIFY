c0736ba4e594c119f186620eb8353238f20f9ba7
butterfly2212 <164756585+Akhankshajupalli@users.noreply.github.com>
2025-02-15 12:24:27 +0530

    Update App.jsx

--- knowtify/src/App.jsx
+++ knowtify/src/App.jsx
@@ -1,4 +1,4 @@
-import { BrowserRouter as Router, Routes, Route } from './react-router-dom';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import './components/styles/Home.css';
 import './components/styles/Header.css';
 import './components/styles/Footer.css';
